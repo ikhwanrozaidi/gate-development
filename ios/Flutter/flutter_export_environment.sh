035418b95bbcc9708de5ebb02e4fdac298fd131a
imanikhwan <ikhwanrozaidi0412@gmail.com>
2024-10-09 16:27:48 +0800

    fucker

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/imanikhwan/fvm/versions/3.19.3"
+export "FLUTTER_APPLICATION_PATH=/Users/imanikhwan/Documents/bloc-to-riverpod/gatepay_convert"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/imanikhwan/Documents/bloc-to-riverpod/gatepay_convert/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=QkFTRV9VUkw9aHR0cHM6Ly91cy1jZW50cmFsMS1nYXRlcGF5LWRiY2YxLmNsb3VkZnVuY3Rpb25zLm5ldA==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yZTRiYTljNmZiNDk5Y2NkNGU4MTQyMDU0Mzc4M2NjNzI2N2FlNDA2Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/imanikhwan/Documents/bloc-to-riverpod/gatepay_convert/.dart_tool/package_config.json"
